68eff2bb212ab8e704e4659057e0ceb48f00b888
cppbitman <daihongjun@kedacom.com>
2018-12-29 00:01:11 +0800

    refactor with conan scheme

--- build.py
+++ build.py
@@ -1,15 +1,5 @@
-from conan.packager import ConanMultiPackager
-from conans import __version__ as conan_version
-from conans.model.version import Version
+#!/usr/bin/env python
+from conanos.build import Main
 
-
-if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    if conan_version < Version("0.99"):
-        builder.add({"os": "Windows", "arch": "x86"}, {}, {}, {})
-        builder.add({"os": "Windows", "arch": "x86_64"}, {}, {}, {})
-    else:
-        builder.add({"os_build": "Windows", "arch_build": "x86"}, {}, {}, {})
-        builder.add({"os_build": "Windows", "arch_build": "x86_64"}, {}, {}, {})
-
-    builder.run()
+if __name__ == "__main__":    
+    Main('nasm',pure_c=True)
